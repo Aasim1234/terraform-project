afa0d5548907110c3831c4334d2d80a078541af1
Aasim1234 <115969237+Aasim1234@users.noreply.github.com>
2023-09-13 15:37:08 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  name               = "aasim-terraform-elb"
+  availability_zones = ["ap-northeast-1a", "ap-northeast-1b"]
 
   listener {
     instance_port     = 80
@@ -39,7 +39,7 @@ resource "aws_elb" "bar" {
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-0f89bdd365c3d966d"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
